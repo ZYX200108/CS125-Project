87c0817a2c0ac7786602f58086b903ac74e1b9b3
ZYX200108 <charlie.zhou828@gmail.com>
2024-01-23 18:49:53 -0800

    Initial Commit

---
+++ CS125/CS125App.swift
@@ -0,0 +1,17 @@
+//
+//  CS125App.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CS125App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS125/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
