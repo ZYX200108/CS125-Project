3306f4a0e9240e9827159606a20569150bae4528
ZYX200108 <charlie.zhou828@gmail.com>
2024-02-06 11:33:57 -0800

    #add Recommandation Tab

---
+++ CS125/RecommandTab.swift
@@ -0,0 +1,18 @@
+//
+//  RecommandTab.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 2/6/24.
+//
+
+import SwiftUI
+
+struct RecommandTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommandTab()
+}
