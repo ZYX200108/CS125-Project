b84da390e3ec4770b3a4f23eaecc3d606011b3fd
ZYX200108 <charlie.zhou828@gmail.com>
2024-02-05 21:30:37 -0800

    #update HealthData Retrival

---
+++ CS125/AddActivity.swift
@@ -0,0 +1,18 @@
+//
+//  AddActivity.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 2/5/24.
+//
+
+import SwiftUI
+
+struct AddActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddActivity()
+}
---
+++ CS125/HealthData.swift
@@ -0,0 +1,8 @@
+//
+//  HealthData.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 2/5/24.
+//
+
+import Foundation
