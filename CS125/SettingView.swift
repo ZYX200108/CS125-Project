36fc4317edcf493248f3180892e7cb14c9f06a74
David Liu <zijiel12@uci.edu>
2024-02-23 11:51:19 -0800

    enable notification and dark mode

--- CS125/SettingView.swift
+++ CS125/SettingView.swift
@@ -4,11 +4,25 @@
 //
 //  Created by Yuxue Zhou on 2/5/24.
 //
+//  Edited by Zijie Liu on 2/23/24.
 
 import SwiftUI
+import UserNotifications
+
+enum Theme: String {
+    case light, dark
+    
+    mutating func toggle() {
+        self = self == .light ? .dark : .light
+    }
+}
 
 struct SettingView: View {
     @ObservedObject public var authViewModel: AuthenticationViewModel
+    @State private var notificationsEnabled = false
+    @State private var showingSettingsAlert = false
+    @State private var openAppSettings = false
+    @AppStorage("themePreference") private var themePreference: Theme = .light
     
     var body: some View {
         VStack {
@@ -29,12 +43,18 @@ struct SettingView: View {
                 }
                 
                 Section(header: Text("Settings")) {
-                    Toggle(isOn: .constant(true)) {
+                    Toggle(isOn: $notificationsEnabled) {
                         Text("Notifications")
                     }
-                    Toggle(isOn: .constant(false)) {
-                        Text("Dark Mode")
+                    .onChange(of: notificationsEnabled) {
+                        if notificationsEnabled {
+                            showCustomAlert()
+                        }
                     }
+                    Toggle("Dark Mode", isOn: Binding(
+                        get: { self.themePreference == .dark },
+                        set: { newValue in self.themePreference = newValue ? .dark : .light }
+                    ))
                 }
                 
                 Section(header: Text("About")) {
@@ -52,7 +72,41 @@ struct SettingView: View {
             Button("Sign Out", action: authViewModel.signOutWithEmail)
                 .padding(.bottom, 20)
         }
+        .alert(isPresented: $showingSettingsAlert) {
+            Alert(
+                title: Text("Notification Permissions"),
+                message: Text("Notifications have been disabled. Please enable them in Settings to continue."),
+                primaryButton: .default(Text("Settings"), action: {
+                    if let url = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(url) {
+                        UIApplication.shared.open(url)
+                    }
+                }),
+                secondaryButton: .cancel{
+                    notificationsEnabled = false
+                }
+            )
+        }
+        .preferredColorScheme(themePreference == .dark ? .dark : .light)
+        
     }
+    
+    private func showCustomAlert() {
+            showingSettingsAlert = true
+        }
+
+    private func requestNotificationsPermission() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if granted {
+                notificationsEnabled = true
+                print("Notification permissions granted.")
+            } else {
+                showingSettingsAlert = true
+                notificationsEnabled = false
+                print("Notification permissions denied.")
+            }
+        }
+    }
+    
 }
 
 
@@ -61,4 +115,15 @@ struct SettingView_Previews: PreviewProvider {
         let authViewModel = AuthenticationViewModel()
         SettingView(authViewModel: authViewModel)
     }
+}
+
+
+struct ExampleView: View {
+    @AppStorage("themePreference") private var themePreference: Theme = .light
+        
+        var body: some View {
+            Text("Hello, World!")
+                .foregroundColor(themePreference == .dark ? .white : .black)
+                .background(themePreference == .dark ? Color.black : Color.white)
+        }
 }
