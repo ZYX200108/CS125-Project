1c022eb609193d3b3b0f90847ba51396b92c3fef
ZYX200108 <charlie.zhou828@gmail.com>
2024-03-13 16:56:15 -0700

    #recommendView update

---
+++ CS125/RecommendationRetrieveModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendationRetrieveModel.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 3/12/24.
+//
+
+import Foundation
---
+++ CS125/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  CS125
+//
+//  Created by Yuxue Zhou on 3/12/24.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
